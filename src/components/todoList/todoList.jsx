220802a1440affe5bb7753f34aa6dbed2c606ef2
IgorKramskoy <ig.kramskoy@it-justice.com>
2023-07-19 20:25:34 +0300

    udate and delete todo

--- src/components/todoList/todoList.jsx
+++ src/components/todoList/todoList.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import {useQuery} from "@apollo/client";
-import {ALL_TODO} from "../../apollo/todos";
+import {useMutation, useQuery} from "@apollo/client";
+import {ALL_TODO, DELETE_TODO, UPDATE_TODO} from "../../apollo/todos";
 const TodoList = () => {
     const {data, error, loading} = useQuery(ALL_TODO);
+    const [toggleTodo, {error: updateError }] = useMutation(UPDATE_TODO);
+    const [removeTodo, {error: removeError}] = useMutation(DELETE_TODO);
 
     if (error) return <h1>Error...</h1>
+    if (updateError) return <h1>Error Update...</h1>
+    if (removeError) return <h1>Error Delete...</h1>
     if (loading) return <h1>Lading...</h1>
 
     return <>
@@ -12,8 +16,25 @@ const TodoList = () => {
             {
                 data.todos.length && data.todos.map((item) =>
                     <li key={item.id}>
-                        <input type='checkbox' defaultChecked={item.completed}/>
+                        <input
+                            type='checkbox'
+                            defaultChecked={item.completed}
+                            onChange={() => toggleTodo({
+                                variables: {
+                                    id: item.id,
+                                    completed: !item.completed,
+                                }
+                            })}
+                        />
                         {item.title}
+                        <button
+                            type='button'
+                            onClick={() => removeTodo({
+                                variables: { id: item.id }
+                            })}
+                        >
+                            X
+                        </button>
                     </li>
                 )
             }
