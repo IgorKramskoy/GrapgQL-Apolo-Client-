e1709414d72bb273efe50c771fe98d2f3fd4123b
IgorKramskoy <ig.kramskoy@it-justice.com>
2023-07-19 20:31:00 +0300

    update cache

--- src/components/todoList/todoList.jsx
+++ src/components/todoList/todoList.jsx
@@ -4,12 +4,24 @@ import {ALL_TODO, DELETE_TODO, UPDATE_TODO} from "../../apollo/todos";
 const TodoList = () => {
     const {data, error, loading} = useQuery(ALL_TODO);
     const [toggleTodo, {error: updateError }] = useMutation(UPDATE_TODO);
-    const [removeTodo, {error: removeError}] = useMutation(DELETE_TODO);
+    const [removeTodo, {error: removeError}] = useMutation(DELETE_TODO, {
+        // Альтернативное обновление Кэша
+        update(cache, { data: { removeTodo } }) {
+            cache.modify({
+                fields: {
+                    allTodos(currentTodos = []) {
+                        return currentTodos.filter(todo => todo.__ref !== `Todo:${removeTodo.id}`)
+                    }
+                }
+            })
+        }
+    });
 
-    if (error) return <h1>Error...</h1>
-    if (updateError) return <h1>Error Update...</h1>
-    if (removeError) return <h1>Error Delete...</h1>
-    if (loading) return <h1>Lading...</h1>
+    if (error) return <h2>Error...</h2>
+    if (updateError) return <h2>Error Update...</h2>
+    if (removeError) return <h2>Error Delete...</h2>
+    if (loading) return <h2>Lading...</h2>
+    if(!data.todos.length) return <h2>No have todos...</h2>
 
     return <>
         <ul>
